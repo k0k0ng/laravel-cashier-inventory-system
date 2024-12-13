f38a5e1c636990084713cee12ad1aefa0ca6f359
JohnRicoMasadas <masadasjohnrico@gmail.com>
2024-12-14 04:28:03 +0800

    Will add notifications UI in header component

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,22 +1,58 @@
 <nav class="flex justify-end items-center gap-4">
   {{-- Notifications --}}
-  <button class="relative hover:text-violet-500">
-    <svg class="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-      fill="currentColor" viewBox="0 0 24 24">
-      <path
-        d="M11.209 3.816a1 1 0 0 0-1.966.368l.325 1.74a5.338 5.338 0 0 0-2.8 5.762l.276 1.473.055.296c.258 1.374-.228 2.262-.63 2.998-.285.52-.527.964-.437 1.449.11.586.22 1.173.75 1.074l12.7-2.377c.528-.1.418-.685.308-1.27-.103-.564-.636-1.123-1.195-1.711-.606-.636-1.243-1.306-1.404-2.051-.233-1.085-.275-1.387-.303-1.587-.009-.063-.016-.117-.028-.182a5.338 5.338 0 0 0-5.353-4.39l-.298-1.592Z" />
-      <path fill-rule="evenodd"
-        d="M6.539 4.278a1 1 0 0 1 .07 1.412c-1.115 1.23-1.705 2.605-1.83 4.26a1 1 0 0 1-1.995-.15c.16-2.099.929-3.893 2.342-5.453a1 1 0 0 1 1.413-.069Z"
-        clip-rule="evenodd" />
-      <path d="M8.95 19.7c.7.8 1.7 1.3 2.8 1.3 1.6 0 2.9-1.1 3.3-2.5l-6.1 1.2Z" />
-    </svg>
-    <span
-      class="absolute top-2 left-5 transform -translate-y-1/2 w-3.5 h-3.5 bg-red-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
-  </button>
+  <div class="dropdown" data-dropdown>
+    <button class="link relative hover:text-violet-500" data-dropdown-button>
+      <svg class="link w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+        fill="currentColor" viewBox="0 0 24 24" data-dropdown-button>
+        <path
+          d="M11.209 3.816a1 1 0 0 0-1.966.368l.325 1.74a5.338 5.338 0 0 0-2.8 5.762l.276 1.473.055.296c.258 1.374-.228 2.262-.63 2.998-.285.52-.527.964-.437 1.449.11.586.22 1.173.75 1.074l12.7-2.377c.528-.1.418-.685.308-1.27-.103-.564-.636-1.123-1.195-1.711-.606-.636-1.243-1.306-1.404-2.051-.233-1.085-.275-1.387-.303-1.587-.009-.063-.016-.117-.028-.182a5.338 5.338 0 0 0-5.353-4.39l-.298-1.592Z" />
+        <path fill-rule="evenodd"
+          d="M6.539 4.278a1 1 0 0 1 .07 1.412c-1.115 1.23-1.705 2.605-1.83 4.26a1 1 0 0 1-1.995-.15c.16-2.099.929-3.893 2.342-5.453a1 1 0 0 1 1.413-.069Z"
+          clip-rule="evenodd" />
+        <path d="M8.95 19.7c.7.8 1.7 1.3 2.8 1.3 1.6 0 2.9-1.1 3.3-2.5l-6.1 1.2Z" />
+      </svg>
+      <span
+        class="link absolute top-2 left-5 transform -translate-y-1/2 w-3.5 h-3.5 bg-red-400 border-2 border-white dark:border-gray-800 rounded-full"
+        data-dropdown-button></span>
+    </button>
+
+    <!-- Dropdown menu -->
+    <div class="dropdown-menu z-10 w-96 divide-y dark:bg-gray-700 dark:divide-gray-600 rounded-lg shadow">
+      <div class="flex justify-between mb-3 px-3">
+        <div class="font-semibold">Notifications</div>
+        <button type="button" class="hover:text-violet-500">
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+            fill="currentColor" viewBox="0 0 24 24">
+            <path d="m3.62 6.389 8.396 6.724 8.638-6.572-7.69-4.29a1.975 1.975 0 0 0-1.928 0L3.62 6.39Z" />
+            <path
+              d="m22 8.053-8.784 6.683a1.978 1.978 0 0 1-2.44-.031L2.02 7.693a1.091 1.091 0 0 0-.019.199v11.065C2 20.637 3.343 22 5 22h14c1.657 0 3-1.362 3-3.043V8.053Z" />
+          </svg>
+        </button>
+      </div>
+      <button class="mb-4 px-4 py-3 text-start text-sm text-white rounded hover:bg-gray-600">
+        <div class="mb-2 font-medium">Lorem Ipsum One</div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mi ipsum, eleifend eget pellentesque ac,
+          lacinia malesuada nisl.</div>
+      </button>
+      <button class="mb-4 px-4 py-3 text-start text-sm text-white rounded hover:bg-gray-600">
+        <div class="mb-2 font-medium">Lorem Ipsum Two</div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mi ipsum, eleifend eget pellentesque ac,
+          lacinia malesuada nisl.</div>
+      </button>
+      <button class="mb-4 px-4 py-3 text-start text-sm text-white rounded hover:bg-gray-600">
+        <div class="mb-2 font-medium">Lorem Ipsum Three</div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mi ipsum, eleifend eget pellentesque ac,
+          lacinia malesuada nisl.</div>
+      </button>
+      <button class="w-full px-4 py-3 text-sm text-white rounded hover:text-violet-300 hover:bg-gray-600">
+        View More
+      </button>
+    </div>
+  </div>
 
 
   {{-- Drop down --}}
-  <div class="dropdown" data-dropdown>
+  <div class=" dropdown" data-dropdown>
     <button type="button" data-dropdown-button
       class="link flex gap-3 px-3 py-2 rounded-md text-gray-950 bg-white hover:bg-violet-800 hover:text-white"
       alt="User dropdown">
@@ -30,7 +66,7 @@
     </button>
 
     <!-- Dropdown menu -->
-    <div class="dropdown-menu z-10 divide-y dark:bg-gray-700 dark:divide-gray-600 rounded-lg shadow w-44 ">
+    <div class="dropdown-menu z-10 w-52 divide-y dark:bg-gray-700 dark:divide-gray-600 rounded-lg shadow">
       <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
         <div class="font-medium">John Doe</div>
         <div>Admin</div>
